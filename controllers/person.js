293b4ddec1397bff854a5b52fb8267186ce13665
sayannath <sayannath235@gmail.com>
2020-12-24 15:15:40 +0530

    Add

--- controllers/person.js
+++ controllers/person.js
@@ -27,7 +27,10 @@ exports.createPerson = (req, res) => {
 
 //Get All Person
 exports.getAllPerson = (req, res) => {
-  Person.find({ userBy: req.params.userId }).exec((err, person) => {
+  Person.find({ userBy: req.params.userId })
+  .populate("userBy")
+  .populate("medicalCondition")
+  .exec((err, person) => {
     if (err) {
       res.status(400).json({
         error: "No PERSONS are found",
